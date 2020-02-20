a5da2a8b08f1fba25b7f794cdd4499e04246985c
sayam sawai <sawaisayam@gmail.com>
2020-02-20 22:58:50 +0530

    Update utils.h

--- utils.h
+++ utils.h
@@ -8,8 +8,8 @@
 
 using namespace std;
 
-#define loop(i,n) for (i = 0; i < n; ++i) 
-#define loop2(i,k,n) for (i = k; i <= n; ++i) 
-#define loopb(i,k,n) for (i = k; i >= n; --i)
+#define loop(n) for (int i = 0; i < n; ++i) 
+#define loop2(k,n) for (int i = k; i <= n; ++i) 
+#define loopb(k,n) for (int i = k; i >= n; --i)
 #define remove_space(x) x.erase(remove(x.begin(), x.end(), ' '), x.end())
 typedef long long ll;
