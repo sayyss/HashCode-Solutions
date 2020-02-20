1f4d50fa254bd023166157629d66470645fca4d0
sayam <sawaisayam@gmail.com>
2020-02-20 22:40:06 +0530

    Added template

---
+++ solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <stack>
+#include <map>
+#include <ctype.h>
+
+using namespace std;
+
+#define loop(i,n) for (i = 0; i < n; ++i) 
+#define loop2(i,k,n) for (i = k; i <= n; ++i) 
+#define loopb(i,k,n) for (i = k; i >= n; --i)
+#define remove_space(x) x.erase(remove(x.begin(), x.end(), ' '), x.end())
+typedef long long ll;
+
+int main(){
+  
+    ios_base::sync_with_stdio(false);
+
+	return 0;
+}
