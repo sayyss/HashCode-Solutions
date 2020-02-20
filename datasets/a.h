31a1aa13f94c7c084be1dd9f0e04071b6a870a14
Nils André-Chang <nils@nilsand.re>
2020-02-20 18:26:59 +0000

    Add a.h

---
+++ datasets/a.h
@@ -0,0 +1,13 @@
+#define B 6
+#define L 2
+#define D 7
+int books[B] = {1,2,3,6,5,4};
+int books_per_library[L] = {5 ,4 ,};
+int signup_duration_per_library[L] = {2 ,3 ,};
+int scan_per_day_per_library[L] = {2,1,};
+#include <vector>
+std::vector<int> books_in_library[L] =
+{
+	{0,1,2,3,4},
+	{0,2,3,5}
+};
